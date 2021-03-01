018a1dfddad32e1dc026ad53850b72eca76f0d08
Beatrice Castagno <beatrice.castagno1@gmail.com>
2021-03-01 22:52:11 +0100

    Fixato problema ripetizione alert

--- controllo_email/js/scripts.js
+++ controllo_email/js/scripts.js
@@ -5,11 +5,16 @@ var emailUtente = prompt("Inserisci la tua email:");
 var email = ["mario.rossi@gmail.com", "marco_ruffini@hotmail.it", "paola.franceschelli@gmail.com", "cesare.aurelio@yahoo.it"];
 
 // scansiono l'array e controllo che l'email non esista già
+var trovata = 0;
 for (var i = 0; i < email.length; i++) {
-  if (emailUtente == email[i]) {
-     alert("Email valida, accesso effettuato.");
-  }
-  else {
+if (emailUtente == email[i]) {
+trovata = 1;
+}
+}
+
+if (trovata == 1) {
+	alert("Email valida, accesso effettuato.");
+}
+else {
     alert("Email non registrata. Effettua la registrazione.");
-  }
 }
